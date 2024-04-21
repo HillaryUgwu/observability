3306c273088824bad55acb9123d7407db09ede48
Hillary Ugwu <ugwuhillaryemeka@gmail.com>
2024-04-21 17:00:49 +0000

    workspace folder path variable added

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,6 +1,11 @@
-cd /workspaces/observability
-source ./bin/set_tf_alias
-source ./bin/install_terraform_cli
-# source ./bin/generate_tfrc_credentials
-# cp $PROJECT_ROOT/terraform.tfvars.example $PROJECT_ROOT/terraform.tfvars
-# source ./bin/build_provider
+echo 'Running the PostCreateCommand...'
+
+mkdir -p ~/.vscode
+cp .devcontainer/keybindings.json ~/.vscode/keybindings.json
+
+cd $CODESPACE_VSCODE_FOLDER
+bash ./bin/set_tf_alias
+bash ./bin/install_terraform_cli
+# bash ./bin/generate_tfrc_credentials
+# cp $CODESPACE_VSCODE_FOLDER/terraform.tfvars.example $CODESPACE_VSCODE_FOLDER/terraform.tfvars
+# bash ./bin/build_provider
