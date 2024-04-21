4a98027b2fccdf291dbacb4e298a724f5c6ad277
Hillary Ugwu <ugwuhillaryemeka@gmail.com>
2024-04-21 18:21:26 +0000

    instances provisioned pending configuration

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 echo 'Running the PostCreateCommand...'
 
 mkdir -p ~/.vscode
--- main.tf
+++ main.tf
@@ -7,6 +7,22 @@ terraform {
   }
 }
 
+data "aws_ami" "amazon_linux_2" {
+ most_recent = true
+
+ filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+ }
+
+ #filter {
+ #   name   = "virtualization-type"
+ #   values = ["hvm"]
+ #}
+
+ owners = ["amazon"]
+}
+
 provider "aws" {
   region = "us-east-1"
 }
@@ -16,10 +32,42 @@ resource "aws_s3_bucket" "terraform_state_bucket" {
   force_destroy = true
 }
 
-# terraform {
-#  backend "s3" {
-#    bucket  = "tfstate-bucket-observe-with-grafana"
-#    key     = "build/terraform.tfstate"
-#    region  = "us-east-1"
-#  }
-#}
+terraform {
+  backend "s3" {
+    bucket  = "tfstate-bucket-observability"
+    key     = "build/terraform.tfstate"
+    region  = "us-east-1"
+  }
+}
+
+variable "instance_tags" {
+  description = "List of tag names for the instances"
+  type        = list(string)
+  default     = ["controller", "node1", "node2"]
+}
+
+resource "aws_instance" "ec2_instances" {
+  count         = 3
+  ami           = data.aws_ami.amazon_linux_2.id
+  instance_type = "t2.micro" 
+  
+  tags = {
+    Name = var.instance_tags[count.index]
+  }
+
+# Conditional execution of the provisioner for the "controller" instance only
+  provisioner "remote-exec" {
+    when    = "controller"
+    inline = [
+      "sudo yum update -y",
+      "sudo yum install ansible -y"
+    ]
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("~/.ssh/id_rsa")
+    host        = self.public_ip
+  }
+}
