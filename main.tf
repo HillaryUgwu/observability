b8ad4c37b581d3b10c1978585c3b92dcea26ba71
Hillary Ugwu <ugwuhillaryemeka@gmail.com>
2024-04-27 21:37:29 +0000

    modify playbook to use simpler config and add ingress rules for Prometheus and Grafana

--- main.tf
+++ main.tf
@@ -58,6 +58,106 @@ data "external" "workspace_ip" {
   program = ["bash", "${path.module}/bin/workspace_ip"]
 }
 
+resource "aws_s3_bucket" "terraform_state_bucket" {
+  bucket = "tfstate-bucket-observability"
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket  = "tfstate-bucket-observability"
+    key     = "build/terraform.tfstate"
+    region  = "us-east-1"
+  }
+}
+
+#variable "instance_tags" {
+#  description = "List of tag names for the instances"
+#  type        = list(string)
+#  default     = ["controller", "node1", "node2"]
+#}
+
+resource "aws_instance" "controller" {
+  ami                     = data.aws_ami.amazon_linux_2023.id
+  instance_type           = "t2.micro"
+  key_name                = aws_key_pair.ssh_key.key_name
+  vpc_security_group_ids  = [aws_security_group.controller_sg.id]
+  user_data               = file("bin/bootstrap_controller")
+
+  tags = {
+    Name                  = "controller"
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = tls_private_key.ssh_key.private_key_openssh
+    host        = self.public_ip
+    host_key    = ""
+  }
+
+  #provisioner "remote-exec" {
+  #  inline = [ "echo '${[for i in aws_instance.node:i.private_ip]}' >> inventory.ini" ]
+  #}
+
+  #provisioner "file" {
+  #  content     = templatefile("${path.module}/src/inventory.ini", { ip_addrs = [self.private_ip] })
+  #  destination = "/path/on/remote/instance/inventory.ini"
+  #}
+
+  # Provisioner to copy the 'src' folder to the remote instance
+  #provisioner "file" {
+  #  source      = "src/" # Ensure there's a trailing slash to copy the contents directly
+  #  destination = "/home/ec2-user"
+  #}
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo '[nodes]' > inventory.ini",
+      "for ip in ${join(" ", [for i in aws_instance.node : i.private_ip])}; do echo $ip >> inventory.ini; done",
+      "echo '${tls_private_key.ssh_key.private_key_openssh}' > /home/ec2-user/id_rsa",
+      "chmod 600 /home/ec2-user/id_rsa",
+      "chown -R ec2-user:ec2-user /home/ec2-user/id_rsa",
+      #"chmod +x /path/on/remote/instance/inventory.ini",
+      "ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook -i inventory.ini ansible/playbook.yml -e 'ansible_ssh_private_key_file=/home/ec2-user/id_rsa'"
+    ]
+  }
+
+  #user_data   = <<-EOF
+  #             #!/bin/bash
+  #             echo '${tls_private_key.ssh_key.private_key_openssh}' > /home/ec2-user/id_rsa
+  #             chmod +x /home/ec2-user/bootstrap_controller
+  #             bash /home/ec2-user/bootstrap_controller
+  #             ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook -i inventory.ini ansible/playbook.yml -e "ansible_ssh_private_key_file=/home/ec2-user/id_rsa"
+  #             EOF
+}
+
+#resource "local_file" "ansible_inventory" {
+#  content = templatefile("src/inventory.ini", {
+#    ip_addrs = [for i in aws_instance.node:i.private_ip]
+#  })
+#  filename = "src/inventory.ini"
+#}
+
+resource "aws_instance" "node" {
+ count                     = 2
+ ami                       = data.aws_ami.amazon_linux_2023.id
+ instance_type             = "t2.micro"
+ key_name                  = aws_key_pair.ssh_key.key_name
+ vpc_security_group_ids    = [aws_security_group.node_sg.id]
+
+ tags = {
+    Name                   = "node-${count.index + 1}"
+ }
+
+ user_data  = <<-EOF
+              #!/bin/bash
+              sudo hostnamectl set-hostname node${count.index + 1}
+              EOF
+}
+
 resource "aws_security_group" "controller_sg" {
  name        = "controller-sg"
  description = "Security group for the controller instance"
@@ -81,125 +181,89 @@ resource "aws_security_group" "controller_sg" {
  }
 }
 
-resource "aws_security_group" "node_sg" {
- name        = "node-sg"
- description = "Security group for the node instances"
+#resource "aws_security_group" "node_sg_old" {
+# name        = "node-sg"
+# description = "Security group for the node instances"
+#
+# ingress {
+#    from_port   = 80
+#    to_port     = 80
+#    protocol    = "tcp"
+#    cidr_blocks = ["0.0.0.0/0"]
+# }
+#
+# ingress {
+#    from_port   = 22
+#    to_port     = 22
+#    protocol    = "tcp"
+#    security_groups = [aws_security_group.controller_sg.id]
+# }
+#
+# egress {
+#    from_port   = 0
+#    to_port     = 0
+#    protocol    = "-1"
+#    cidr_blocks = ["0.0.0.0/0"]
+# }
+#
+# tags = {
+#    Name = "node-sg"
+# }
+#}
 
- ingress {
+resource "aws_security_group" "node_sg" {
+  name        = "node-sg"
+  description = "Security group for the node instances"
+
+  ingress {
+    description = "Allow SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    security_groups = [aws_security_group.controller_sg.id]
+  }
+  ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
  }
-
- ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    security_groups = [aws_security_group.controller_sg.id]
- }
-
- egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
- }
-
- tags = {
-    Name = "node-sg"
- }
-}
-
-resource "aws_s3_bucket" "terraform_state_bucket" {
-  bucket = "tfstate-bucket-observability"
-  lifecycle {
-    prevent_destroy = true
+  ingress {
+    description = "Allow traffic to Node exporter server (port 9100 by default)"
+    from_port        = 9100
+    to_port          = 9100
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
-}
-
-terraform {
-  backend "s3" {
-    bucket  = "tfstate-bucket-observability"
-    key     = "build/terraform.tfstate"
-    region  = "us-east-1"
+  ingress {
+    description = "Allow traffic to Prometheus server (port 9090 by default)"
+    from_port        = 9090
+    to_port          = 9090
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
-}
-
-variable "instance_tags" {
-  description = "List of tag names for the instances"
-  type        = list(string)
-  default     = ["controller", "node1", "node2"]
-}
-
-resource "aws_instance" "controller" {
-  ami                     = data.aws_ami.amazon_linux_2023.id
-  instance_type           = "t2.micro"
-  key_name                = aws_key_pair.ssh_key.key_name
-  vpc_security_group_ids  = [aws_security_group.controller_sg.id]
+  ingress {
+    description = "Allow traffic to Grafana (port 3000 by default)"
+    from_port        = 3000
+    to_port          = 3000
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  egress {
+    description = "Allow all outbound traffic"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
   tags = {
-    Name                  = "controller"
-  }
-
-  connection {
-    type        = "ssh"
-    user        = "ec2-user"
-    private_key = tls_private_key.ssh_key.private_key_openssh
-    host        = self.public_ip
-    host_key    = ""
-  }
-
-  #provisioner "local-exec" {
-  #  command = "bash ${path.module}/bin/create_inventory_skeleton_file"
-  #}
-
-  # Provisioner to copy the 'src' folder to the remote instance
-  provisioner "file" {
-    source      = "src/" # Ensure there's a trailing slash to copy the contents directly
-    destination = "/home/ec2-user"
-  }
-
-  #provisioner "remote-exec" {
-  #  inline = [
-  #    "echo '${tls_private_key.ssh_key.private_key_openssh}' > /home/ec2-user/id_rsa",
-  #    "chmod +x /home/ec2-user/bootstrap_controller",
-  #    "bash /home/ec2-user/bootstrap_controller",
-  #    "ansible-playbook -i inventory.ini ansible/playbook.yml -e 'ansible_ssh_private_key_file=/home/ec2-user/id_rsa'"
-  #  ]
-  #}
-
-  user_data   = <<-EOF
-               #!/bin/bash
-               echo '${tls_private_key.ssh_key.private_key_openssh}' > /home/ec2-user/id_rsa
-               chmod +x /home/ec2-user/bootstrap_controller
-               bash /home/ec2-user/bootstrap_controller
-               ansible-playbook -i inventory.ini ansible/playbook.yml -e "ansible_ssh_private_key_file=/home/ec2-user/id_rsa"
-               EOF
-}
-
-resource "local_file" "ansible_inventory" {
-  content = templatefile("src/inventory.ini", {
-    ip_addrs = [for i in aws_instance.node:i.private_ip]
-  })
-  filename = "src/inventory.ini"
-}
-
-resource "aws_instance" "node" {
- count                     = 2
- ami                       = data.aws_ami.amazon_linux_2023.id
- instance_type             = "t2.micro"
- key_name                  = aws_key_pair.ssh_key.key_name
- vpc_security_group_ids    = [aws_security_group.node_sg.id]
-
- tags = {
-    Name                   = "node-${count.index + 1}"
+    Name = "node-sg"
  }
-
- user_data  = <<-EOF
-              #!/bin/bash
-              sudo hostnamectl set-hostname node${count.index + 1}
-              EOF
 }
 
 output "controller_private_ip" {
